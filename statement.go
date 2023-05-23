df25fcd23bdd6212b329b4b921cee261c5cdf5e8
Maddie Schipper <me@maddiesch.com>
2023-05-23 11:09:42 -0500

    Add Statement Helper functions

--- statement.go
+++ statement.go
@@ -6,6 +6,33 @@ import (
 	"github.com/maddiesch/go-raptor/statement/generator"
 )
 
+func QueryRowStatement(ctx context.Context, q Querier, stmt generator.Generator) *Row {
+	query, args, err := stmt.Generate()
+	if err != nil {
+		return &Row{err: err}
+	}
+
+	return q.QueryRow(ctx, query, args...)
+}
+
+func QueryStatement(ctx context.Context, q Querier, stmt generator.Generator) (*Rows, error) {
+	query, args, err := stmt.Generate()
+	if err != nil {
+		return nil, err
+	}
+
+	return q.Query(ctx, query, args...)
+}
+
+func ExecStatement(ctx context.Context, e Executor, stmt generator.Generator) (Result, error) {
+	query, args, err := stmt.Generate()
+	if err != nil {
+		return nil, err
+	}
+
+	return e.Exec(ctx, query, args...)
+}
+
 func (c *Conn) QueryRowStatement(ctx context.Context, statement generator.Generator) *Row {
 	query, args, err := statement.Generate()
 	if err != nil {
@@ -22,4 +49,13 @@ func (c *Conn) QueryStatement(ctx context.Context, statement generator.Generator
 	}
 
 	return c.Query(ctx, query, args...)
+}
+
+func (c *Conn) ExecStatement(ctx context.Context, statement generator.Generator) (Result, error) {
+	query, args, err := statement.Generate()
+	if err != nil {
+		return nil, err
+	}
+
+	return c.Exec(ctx, query, args...)
 }
