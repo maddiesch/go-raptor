d65bb37594e6e693820f63c4411fd6ebf2e38653
Maddie Schipper <me@maddiesch.com>
2023-12-21 18:51:47 -0600

    Adding SLog Logger

--- raptor.go
+++ raptor.go
@@ -7,6 +7,8 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"log/slog"
+	"strconv"
 	"sync"
 	"sync/atomic"
 
@@ -94,6 +96,37 @@ func NewNoopQueryLogger() QueryLogger {
 }
 
 func (w *noopQueryLogger) LogQuery(context.Context, string, []any) {}
+
+type SLogLoggerFunc func(context.Context, string, ...any)
+
+func NewSlogFuncQueryLogger(fn SLogLoggerFunc) QueryLogger {
+	return &fnQueryLogger{fn}
+}
+
+type fnQueryLogger struct {
+	fn SLogLoggerFunc
+}
+
+func (l *fnQueryLogger) LogQuery(ctx context.Context, q string, params []any) {
+	args := make([]any, len(params))
+
+	for i, a := range params {
+		switch a := a.(type) {
+		case slog.Attr:
+			args[i] = a
+		case sql.NamedArg:
+			args[i] = slog.Any(a.Name, a.Value)
+		case string:
+			args[i] = slog.String(strconv.FormatInt(int64(i), 10), a)
+		case int64:
+			args[i] = slog.Int64(strconv.FormatInt(int64(i), 10), a)
+		default:
+			args[i] = slog.Any(strconv.FormatInt(int64(i), 10), a)
+		}
+	}
+
+	l.fn(ctx, q, slog.Group("params", args...))
+}
 
 // A Result summarizes an executed SQL command.
 type Result interface {
