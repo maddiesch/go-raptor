3b46b2f8bcb2be6b59d10a349ebd0b91f84d4195
Maddie Schipper <me@maddiesch.com>
2024-01-07 14:33:15 -0600

    Add Duplicate Migration Unit Test

--- migrate/migrate_test.go
+++ migrate/migrate_test.go
@@ -12,25 +12,56 @@ import (
 )
 
 func TestUp(t *testing.T) {
-	db, err := raptor.New(":memory:?mode=memory&cache=shared")
-	require.NoError(t, err)
+	t.Run("given a migration that has not been run", func(t *testing.T) {
+		db, err := raptor.New(":memory:?mode=memory&cache=shared")
+		require.NoError(t, err)
+		t.Cleanup(func() {
+			db.Close()
+		})
 
-	db.SetLogger(raptor.NewQueryLogger(os.Stderr))
+		db.SetLogger(raptor.NewQueryLogger(os.Stderr))
 
-	m := migrate.Migration{
-		Name: "testing-migration",
-		Up: []string{
-			`CREATE TABLE "migration_table" ("example" TEXT);`,
-		},
-	}
+		m := migrate.Migration{
+			Name: "testing-migration",
+			Up: []string{
+				`CREATE TABLE "migration_table" ("example" TEXT);`,
+			},
+		}
 
-	err = migrate.Up(context.Background(), db, m)
-	require.NoError(t, err)
+		err = migrate.Up(context.Background(), db, m)
+		require.NoError(t, err)
+	})
+
+	t.Run("given a migration that has already been executed", func(t *testing.T) {
+		db, err := raptor.New(":memory:?mode=memory&cache=shared")
+		require.NoError(t, err)
+		t.Cleanup(func() {
+			db.Close()
+		})
+
+		db.SetLogger(raptor.NewQueryLogger(os.Stderr))
+
+		m := migrate.Migration{
+			Name: "testing-migration-dup",
+			Up: []string{
+				`CREATE TABLE "migration_table" ("example" TEXT);`,
+			},
+		}
+
+		err = migrate.Up(context.Background(), db, m)
+		require.NoError(t, err)
+
+		err = migrate.Up(context.Background(), db, m)
+		require.NoError(t, err)
+	})
 }
 
 func TestDown(t *testing.T) {
 	db, err := raptor.New(":memory:?mode=memory&cache=shared")
 	require.NoError(t, err)
+	t.Cleanup(func() {
+		db.Close()
+	})
 
 	db.SetLogger(raptor.NewQueryLogger(os.Stderr))
 
